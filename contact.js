2862ee9b71afe7d5159a3a98f64c028b4fe76a79
xkafeier <xkafeier@gmail.com>
2026-07-18 13:47:35 +1000

    feat: initial design of nav bar

---
+++ contact.js
@@ -0,0 +1,9 @@
+function navbar() {
+    // Opens the side navigation by setting its width to 250px
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNavbar() {
+    // Closes the side navigation by setting its width back to 0
+    document.getElementById("mySidenav").style.width = "0";
+}
